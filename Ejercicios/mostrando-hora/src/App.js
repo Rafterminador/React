fee57bb1755853f2130c090b4f044d03b796b4fc
fernando <ferrafterminador@gmail.com>
2021-11-20 22:37:47 -0600

    ejecutando render de react a cada cierto tiempo

---
+++ Ejercicios/mostrando-hora/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <p>Hola Mundo</p>
+      <input></input>
+      <pre>{(new Date).toLocaleTimeString()}</pre>
+    </div>
+  );
+}
+
+export default App;
---
+++ Ejercicios/mostrando-hora/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+const render = () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );  
+}
+setInterval(render, 1000)
