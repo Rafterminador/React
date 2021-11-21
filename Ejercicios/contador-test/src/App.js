650efda6252650c6a842e063e50c1f5d22256de5
fernando <ferrafterminador@gmail.com>
2021-11-20 21:59:52 -0600

    rehusando componentes

--- Ejercicios/contador-test/src/App.js
+++ Ejercicios/contador-test/src/App.js
@@ -1,13 +1,21 @@
 import { useState } from "react";
+function Button(props){
+  return (
+    <button onClick={ () => props.onClickFunction(props.message)}>
+        +{props.message}
+    </button>
+  )
+}
 function Contador() {
   const [counter, setCounter] = useState(0)
-  const handleClick = () => setCounter(counter+1)
+  const handleClick = (increment) => setCounter(counter+increment)
   return (
     <div>
-      <button onClick={handleClick}>
-        +1
-      </button>
-      <div>{counter+1}</div>
+      <Button onClickFunction = {handleClick} message = {1}/>
+      <Button onClickFunction = {handleClick} message = {5}/>
+      <Button onClickFunction = {handleClick} message = {10}/>
+      <Button onClickFunction = {handleClick} message = {15}/>
+      <div>{counter}</div>
     </div>
   ) 
 }
