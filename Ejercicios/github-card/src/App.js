f29317c532fadcb462888e714676661e749755b9
fernando <ferrafterminador@gmail.com>
2021-11-21 13:58:04 -0600

    leyendo datos de la API de github

--- Ejercicios/github-card/src/App.js
+++ Ejercicios/github-card/src/App.js
@@ -1,18 +1,33 @@
 import React from "react";
 import "./App.css"
+
+
+const testData = [
+    {name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
+    {name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
+    {name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
+]
+
 class Card extends React.Component{
   render(){
+    const profile = this.props
     return(
       <div className="github-profile">
-        <img src="https://via.placeholder.com/75" alt="only for fill purposes"/>
+        <img src={profile.avatar_url} alt="profile avatar"/>
         <div className="info">
-          <div className="name">Name here....</div>
-          <div className="company">Company here...</div>
+          <div className="name">{profile.name}</div>
+          <div className="company">{profile.company}</div>
         </div>
       </div>
     )
   }
 }
+
+const CardList = (props) => (
+  <div>
+    {testData.map(profile => <Card {...profile}/>)}
+  </div>
+)
 
 class App extends React.Component{
   render(){
@@ -21,7 +36,7 @@ class App extends React.Component{
         <div className = "header">
           <h1>{this.props.title}</h1>
         </div>
-        <Card />
+        <CardList />
       </div>
     )
   }
