45b83ee3c145acd8818f565f7d9c6efa9037a8f4
fernando <ferrafterminador@gmail.com>
2022-07-26 21:27:21 -0600

    texto en suspensivo para textos grandes

--- Curso de Introducción a React.js/ejercicios/src/components/Card.jsx
+++ Curso de Introducción a React.js/ejercicios/src/components/Card.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Pin } from "./Pin"
+import { Text, StyleSheet } from "react-native";
 const Card = ({ props }) => {
     console.log(props)
     return (
         <div className='grid grid-cols-12'>
             <div className='col-span-8'>
                 <h2>{props.title}</h2>
-                <p>{props.text}</p>
+                <Text style={styles.paragraph} numberOfLines={5} className="card-text"> {props.text}</Text>
             </div>
             <div className='col-span-4'>
                 <div className='flex flex-col items-end'>
@@ -19,5 +20,11 @@ const Card = ({ props }) => {
         </div>
     )
 }
+
+const styles = StyleSheet.create({
+    paragraph: {
+        textAlign: "justify",
+    },
+});
 
 export { Card }
